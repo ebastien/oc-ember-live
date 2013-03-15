b15fce7c8a49089d1178f904d1e3c2c960718b0e
Emmanuel Bastien <os@ebastien.name>
2013-03-15 14:37:06 +0100

    Blank Ember application.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require bootstrap
+//= require d3
+//= require nv.d3
+//= require_self
+
+Demo = Ember.Application.create();
